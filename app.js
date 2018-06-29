53138bd1d0d4bc73fbcba7d60ed2bbf4becb9590
Jack Ryan <jack.dixon.ryan@gmail.com>
2018-06-29 10:33:04 -0600

    Added a global clock that nearly works, so user's can compare times against arrival times. But comparing user input for times against the global clock may prove difficult.

--- app.js
+++ app.js
@@ -5,6 +5,50 @@ var trainDestination;
 var trainTime;
 var trainFrequency;
 
+//Defining a variable based on the current time
+var d = new Date();
+console.log(d);
+
+//Dividing it into the components we actually need
+var hours = d.getHours();
+var minutes = d.getMinutes();
+var seconds = d.getSeconds();
+
+//global clock is an interval that transpires every second;
+var globalClock = setInterval(function() {
+
+    //Adds one second to the clock every second...
+    seconds++;
+
+    //Evaluates based on our time scale to ensure the clock isn't all wonky.
+    if (seconds > 59) {
+        minutes++;
+        seconds = 00;
+    }
+    if (minutes > 59) {
+        hours++;
+        minutes = 00;
+    }
+
+    //Concatenates to display as the proper format. Need to find a means of showing the 0 ahead of single-digit numbers, but my attempts to do so have proven ineffective.
+    var currentTime = hours + ":" + minutes + ":" + seconds;
+
+    //To the DOM with you!
+    $("#current-time").text(currentTime);
+
+}, 1000);
+
+//Will go in the table, eventually.
+var minutesAway;
+var hoursAway;
+
+//now to compare our trains' arrival dates against the global clock
+var nextArrivals = setInterval(function() {
+    minutesAway 
+
+});
+
+
 //On clicking submit...
 $("#submit").on("click", function() {
     //Updating initial variables with user inputs. 
