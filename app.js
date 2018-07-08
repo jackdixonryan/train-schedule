e567e0f49ec7a9786d425f9ce9e1218ec2d85a0c
Jack Ryan <jack.dixon.ryan@gmail.com>
2018-07-07 18:19:33 -0600

    Fixed the wait time bug. Code is now functioning correctly.

--- app.js
+++ app.js
@@ -1,81 +1,86 @@
+//Initializing the Firebase configuration.
+var config = {
+    apiKey: "AIzaSyB119InCHHmAJR6v4KpCZe08lgTV6eEbsk",
+    authDomain: "timesheet-99bd6.firebaseapp.com",
+    databaseURL: "https://timesheet-99bd6.firebaseio.com",
+    projectId: "timesheet-99bd6",
+    storageBucket: "timesheet-99bd6.appspot.com",
+    messagingSenderId: "985761687542"
+};
 
-//Declaring initial variables for user inputs
+firebase.initializeApp(config);
+
+//database assigned to a database variables. 
+var database = firebase.database();
+
+//Declaring initial variables for user inputs and for the current time.
 var trainName;
 var trainDestination;
 var trainTime;
 var trainFrequency;
 
-//Defining a variable based on the current time
-var d = new Date();
-console.log(d);
 
-//Dividing it into the components we actually need
-var hours = d.getHours();
-var minutes = d.getMinutes();
-var seconds = d.getSeconds();
+//On referencing the database...
+database.ref().on("child_added", function(snapshot) {
 
-//global clock is an interval that transpires every second;
-var globalClock = setInterval(function() {
+    var newTime = moment();
+    console.log("New current time", newTime);
+    var newTimeObj = moment(snapshot.val().firstTime, "hh:mm");
+    console.log("New First Time: ", newTimeObj)
+    var newDifference = newTime.diff(newTimeObj, 'minutes');
+    console.log("Time between there and now:", newDifference)
 
-    //Adds one second to the clock every second...
-    seconds++;
+    var newWaitTime = snapshot.val().frequency - (newDifference % snapshot.val().frequency);
+    //snapshot.val().timeUntilNext = newWaitTime
+    console.log("Your new wait time is", newWaitTime)
 
-    //Evaluates based on our time scale to ensure the clock isn't all wonky.
-    if (seconds > 59) {
-        minutes++;
-        seconds = 00;
-    }
-    if (minutes > 59) {
-        hours++;
-        minutes = 00;
-    }
+    var newTableRow = $("<tr>");
 
-    //Concatenates to display as the proper format. Need to find a means of showing the 0 ahead of single-digit numbers, but my attempts to do so have proven ineffective.
-    var currentTime = hours + ":" + minutes + ":" + seconds;
+    database.ref().update({
+        timeUntilNext: newWaitTime
+    });
 
-    //To the DOM with you!
-    $("#current-time").text(currentTime);
+    $(newTableRow).html("<th scope='row'>" + snapshot.val().name + "</th><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().frequency + "</td><td>" + snapshot.val().firstTime + "</td><td>" + newWaitTime + "</td>");
 
-}, 1000);
-
-//Will go in the table, eventually.
-var minutesAway;
-var hoursAway;
-
-//now to compare our trains' arrival dates against the global clock
-var nextArrivals = setInterval(function() {
-    minutesAway 
+    $("#table").append(newTableRow);
 
 });
 
-
 //On clicking submit...
 $("#submit").on("click", function() {
-    //Updating initial variables with user inputs. 
+
     trainName = $("#name").val()
     trainDestination = $("#destination").val()
     trainTime = $("#first-time").val()
-    trainFrequency = $("#frequency").val()
+    trainFrequency = $("#frequency").val();
+
+    var time = moment();
+    //Take the first train time and set it to a moment.
+    var firstTimeObj = moment(trainTime, "hh:mm");
+    console.log(firstTimeObj)
+    //Take the different between then and now. 
+    var difference = time.diff(firstTimeObj, 'minutes');
+    //Correct the difference figure if it's in the future rather than in the past (eg, if it returns a negative value).
+    if (difference < 0) {
+        difference = difference + 1440;
+    }
+    //Calculate the time until the next train based on the frequency of the train's running subtracting the difference between that time and now.
+    var timeUntil = trainFrequency - (difference % trainFrequency);
+    console.log(timeUntil)
+
+
+    database.ref().push({
+        name: trainName,
+        firstTime: trainTime,
+        destination: trainDestination,
+        frequency: trainFrequency,
+        timeUntilNext: timeUntil
+    });
 
     //Emptying fields for better UX
     $("#name").val("");
     $("#destination").val("");
     $("#first-time").val("");
     $("#frequency").val("");
-
-    //For bug checking--checkeroo
-    console.log(trainName);
-    console.log(trainDestination);
-    console.log(trainTime);
-    console.log(trainFrequency);
-
-    //creating a new row element for the table.
-    var newTableRow = $("<tr>");
-
-    //Now all at once, add the input information to the new table row.
-    $(newTableRow).html("<th scope='row'>" + trainName + "</th><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + trainTime);
-
-    //Checkeroo, except that the form doesn't perfectly align with the data needed to populate the table. That comes later.
-    $("#table").append(newTableRow);
 
 });
